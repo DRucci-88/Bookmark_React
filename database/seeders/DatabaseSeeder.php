51ebf34a3eb3d59393f610004b87af930b905380
DRucci-88 <60127414+DRucci-88@users.noreply.github.com>
2022-01-18 01:46:20 +0700

    npm run production for minified using laravel mix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+//         \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'name' => 'Robert Junior',
+            'email' => 'robert@gmail.com',
+            'password' => \bcrypt('robert'),
+        ]);
     }
 }
