ddde6b1946bc8c5d2d1680e39a609b12cca8004b
DRucci-88 <60127414+DRucci-88@users.noreply.github.com>
2022-01-19 01:06:01 +0700

    Preparing typescript tsc --init --jsx react

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,26 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes React and other helpers. It's a great starting point while
- * building robust, powerful web applications using React + Laravel.
- */
-
 require('./bootstrap');
 
-/**
- * Next, we will create a fresh React component instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+import React from 'react'
+import { render } from 'react-dom'
 
-require('./components/Example');
+import { InertiaApp} from "@inertiajs/inertia-react";
+
+const app = document.getElementById('app');
+
+render(
+    <InertiaApp initialPage={JSON.parse(app.dataset.page)}
+                initialComponent={}
+                resolveComponent={name => import(`./Pages/${name}`).then(module => module.default)}
+                />,
+    app
+)
+
+// import { createInertiaApp } from '@inertiajs/inertia-react'
+
+//
+// createInertiaApp({
+//     resolve: name => require(`./Pages/${name}`),
+//     setup({el, App, props}) {
+//         render(<App {...props} />, el)
+//     },
+// }).then(r => '')
